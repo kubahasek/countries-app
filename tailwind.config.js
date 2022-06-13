39720d59ce695eab1cc15ff2a9b3d8630d1899a4
Jakub Hašek <hasek.kuba@hotmail.cz>
2022-06-13 16:59:15 +0200

    added colors to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   content: ["./app/**/*.{ts,tsx,jsx,js}"],
+  darkMode: "class",
   theme: {
+    colors: {
+      darkblue: "hsl(209, 23%, 22%)",
+      darkbg: "hsl(207, 26%, 17%)",
+      darktext: "hsl(200, 15%, 8%)",
+      darkgray: "hsl(0, 0%, 52%)",
+      lightgray: "hsl(0, 0%, 98%)",
+      white: "hsl(0, 0%, 100%)",
+    },
     extend: {},
   },
   plugins: [],
