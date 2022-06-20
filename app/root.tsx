5957bb296e622f51116ba25861b3fb96911c17ee
Jakub Hašek <hasek.kuba@hotmail.cz>
2022-06-20 21:24:43 +0200

    updated title

--- app/root.tsx
+++ app/root.tsx
@@ -13,7 +13,7 @@ import {
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "Countries App",
   viewport: "width=device-width,initial-scale=1",
 });
 
